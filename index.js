793d192f3dccd40b313894003b6bdfeab2a2e078
atBeatty <adam.t.beatt@gmail.com>
2020-11-17 15:54:58 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    console.log(dodger.style.left)
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+      let leftBorder = dodger.style.left.replace("px", "")
+      let left = parseInt(leftBorder, 10);
+      console.log(leftBorder)
+
+      if (left < 400) {
+          dodger.style.left = `${left + 1}px`;
+      }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
