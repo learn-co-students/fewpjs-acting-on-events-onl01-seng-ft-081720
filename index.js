7654d7121db1786a503d32139eb36198a9906d91
Eli Cusic <tntitans31@gmail.com>
2020-11-19 07:40:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    // let rightNumbers = dodger.style.left.replace("px", "")
+    // let right = parseInt(rightNumbers, 10)
+
+    // console.log(right)
+    // console.log(rightNumbers)
+
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    console.log(left)
+    console.log(leftNumbers)
+
+    if (left < 360) {
+        console.log('fuck you')
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    console.log(e.key)
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+});
